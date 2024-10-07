7fe8d120ff2122253cb83cc1e5fe44d8d39d219e
Abdumalik-Abdullayev1 <abdumalikabdullayev10@gmail.com>
2024-10-07 16:15:20 +0500

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+// task 35
+// function multipltValues (obj: Record<string, number>, n:number): Record<string, number> {
+//     const result: Record<string, number> = {}
+//     for(const key in obj){
+//         if(obj.hasOwnProperty(key)){
+//             result[key] = obj[key] * n;
+//         }
+//     }
+//     return result
+// }
+// const n = 5
+// const obj = {a: 2, b: 3, c: 4}
+// const res = multipltValues(obj, n)
+// console.log(res);
+
+
+
